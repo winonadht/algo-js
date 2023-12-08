0653850bbefee310d00fe0401fd6d30b0db3abb4
Winona <winonal.dehaut@gmail.com>
2023-12-08 15:58:44 +0100

    js-winona

---
+++ 3.2.js
@@ -0,0 +1,10 @@
+
+let tab = [1,2,3,4,5];
+    let sum2 = 0;
+
+    for (let i = 0; i < tab.length; i++) {
+  sum2 += tab[i];
+
+    }
+    let moyenne = (sum2/100)*(tab.length);
+console.log(moyenne);
