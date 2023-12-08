7a3f941239ce51395dd129bb1c557582a8ec4548
Winona <winonal.dehaut@gmail.com>
2023-12-08 10:18:59 +0100

    js2.4-5

--- 2.4.js
+++ 2.4.js
@@ -1,6 +1,11 @@
-for (let i = 1; i <= 100; i += 1) {
-    console.log(i);
-  }
-  for (let i = 2; i <= 100; i += 2) {
-    console.log(i/2);
+
+for (let i = 1; i <= 100; i += 1) 
+{
+  if (i%2 ==0) {
+  console.log(i/2)
+  } 
+  if (i%2 !=1) {
+  console.log(i*3)
+}
+     
   }
---
+++ 2.5.js
@@ -0,0 +1,6 @@
+let number = prompt ('inscrivez votre nombre préféré');
+
+ while ( number != 42) {
+    console.log( ' Êtes-vous sûr? ');
+    number = prompt('inscrivez votre nombre préféré');
+ }
