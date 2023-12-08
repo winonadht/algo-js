c3ab8d68b0361adae78baef648c582114f6cb322
Winona <winonal.dehaut@gmail.com>
2023-12-08 12:03:44 +0100

    3.0.js

---
+++ 3.0.js
@@ -0,0 +1,3 @@
+let arr = ["alexis", "alice", "aurelien", "cassidy", "Damien", "Denis", "Giuseppe", "hanen", "huseyn", "jeremy", "joshua", "julien", "nicolas", "maoro", "marvin", "noé", "otman", "tess", "thomas", "ugur", "winona"];
+for (let elem of arr) 
+  console.log(elem); 
